0b0a730d245b142c09e9734931cf053ac8f3df96
MohanKumarMurugan <m.mohankumar2626@gmail.com>
2025-06-16 22:07:55 +0530

    Redirection has be setuped without bug

--- myapp/blog/urls.py
+++ myapp/blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name='index'),
     path("post/<int:post_id>", views.detail, name='detail'),
-    path("new-url", views.new_url_view, name='new_url'),
-    path("old-url", views.old_url_redirect, name='old_url'),
+    path("new_url", views.new_url_view, name='new_url'),
+    path("old_url", views.old_url_redirect, name='old_url'),
 ]
